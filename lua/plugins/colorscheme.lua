7258d6fd635f45249d99f9f33568b10d6537dc61
visualect <visualection@gmail.com>
2024-03-06 21:06:58 +0200

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,11 @@
 return {
-  'ellisonleao/gruvbox.nvim',
+  'p00f/alabaster.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require('gruvbox').setup {
-      contrast = 'hard',
-    }
-    vim.cmd.hi 'Comment gui=none'
+    vim.opt.termguicolors = true
     vim.o.background = 'dark' -- or "light" for light mode
-    vim.cmd [[colorscheme gruvbox]]
+    vim.cmd [[colorscheme alabaster]]
+    vim.cmd.hi 'Comment gui=none'
   end,
 }
