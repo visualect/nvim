e98a83b855a1c821702eeb5706e78f491afe4c22
visualect <visualection@gmail.com>
2024-03-06 12:12:21 +0200

    Decomposition

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,28 @@
+require('lazy').setup {
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- `opts = {}` is equivalent to `require('Comment').setup({})`
+  { 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment visual regions/lines
+
+  require 'plugins.gitsigns',
+
+  require 'plugins.which-key',
+
+  require 'plugins.telescope',
+
+  require 'plugins.nvim-tree',
+
+  require 'plugins.lsp',
+
+  require 'plugins.conform',
+
+  require 'plugins.cmp',
+
+  require 'plugins.colorscheme',
+
+  require 'plugins.mini',
+
+  require 'plugins.tree-sitter',
+
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
