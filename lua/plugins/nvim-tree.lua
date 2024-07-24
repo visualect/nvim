45fa87de2c4fb05a147903dc232544bfeac149ff
foma <foma@mirai.com>
2024-07-24 18:48:34 +0200

    Fix bugs

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,8 +1,15 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
   lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
-    require('nvim-tree').setup {}
+    local api = require('nvim-tree.api')
+    vim.keymap.set('n', '<leader>tt', api.tree.toggle, { desc = '[T]oggle [T]ree' })
+
+    require("nvim-tree").setup {
+    }
   end,
 }
