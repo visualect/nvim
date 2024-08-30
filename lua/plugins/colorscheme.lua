82456d3c94b08ef8d65fa2a0521df3991722dbf4
foma <foma@mirai.com>
2024-08-30 23:02:55 +0200

    Minor update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "tjdevries/colorbuddy.nvim",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     vim.opt.termguicolors = true
-    vim.o.background = 'dark' -- or "light" for light mode
-    vim.cmd [[colorscheme gruvbox]]
-    require("gruvbox").setup({
-      contrast = 'hard'
-    })
+    vim.cmd [[colorscheme gruvbuddy]]
   end,
 }
