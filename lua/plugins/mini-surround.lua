c489ae9be9ef8b23e4b7ced38bfcbd27db05945a
ian <yanikidev@gmail.com>
2025-06-01 11:40:37 +0200

    Add colorizer and autotag

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -3,13 +3,6 @@ return {
     "echasnovski/mini.surround",
     version = false,
     config = function()
-      -- - Add surrounding with `sa` (in visual mode or on motion).
-      -- - Delete surrounding with `sd`.
-      -- - Replace surrounding with `sr`.
-      -- - Find surrounding with `sf` or `sF` (move cursor right or left).
-      -- - Highlight surrounding with `sh`.
-      -- - Change number of neighbor lines with `sn` (see |MiniSurround-algorithm|).
-
       require("mini.surround").setup()
     end,
   },
