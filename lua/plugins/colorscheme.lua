1833537aeda39ae21c98a602ac3ec3b6822ea3c1
ian <yanikidev@gmail.com>
2025-06-09 21:57:52 +0200

    remove statuscolumn

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,12 @@
 return {
-  -- {
-  --   "p00f/alabaster.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme("alabaster")
-  --   end,
-  -- },
   {
+    --   "p00f/alabaster.nvim",
     "amedoeyes/eyes.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
+      --     vim.cmd.colorscheme("alabaster")
       vim.cmd.colorscheme("eyes")
     end,
   },
